50b1d8c5017bc25b1e3eee2d966acb0a07d47015
Matt Hess <1355967+mhess@users.noreply.github.com>
2025-05-15 11:47:13 -0600

    Add vercels remix package and use vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite";
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+	plugins: [remix(), tsconfigPaths(), vercelPreset()],
 });
