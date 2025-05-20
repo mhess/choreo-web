4235c68440aa9093de3fcd6440e7b7eb336342d8
Matt Hess <1355967+mhess@users.noreply.github.com>
2025-05-20 14:48:08 -0600

    Redo linting. Add ts and tailwind lang servers
    * Install lsp-config compatible elint lang server
    * Install ts lang server
    * Remove tailind eslint plugin bc no support for tailwind v4
    * Use official tailwind lang server

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,13 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import tailwind from "eslint-plugin-tailwindcss";
 
-export default [
-	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+export default tseslint.config(
 	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	...tailwind.configs["flat/recommended"],
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
 	// https://github.com/jsx-eslint/eslint-plugin-react/#configuration-new-eslintconfigjs
 	pluginReact.configs.flat.recommended,
-	pluginReact.configs.flat["jsx-runtime"],
-	{ settings: { tailwindcss: { tags: ["tw"] } } },
-];
+	pluginReact.configs.flat["jsx-runtime"]
+);
